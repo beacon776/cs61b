75d84dfdbe0bcb9af2b11b0fbf5225eb20cb8e18
beacon776 <3214984261@qq.com>
2024-11-12 09:02:20 +0800

    Update HelloNumbers.java

--- lab1/HelloNumbers.java
+++ lab1/HelloNumbers.java
@@ -8,4 +8,23 @@ public class HelloNumbers {
             x = x + 1;
         }
 	}
-} 
+}
+public static class Walrus{
+    public int weight;
+    public double  tuskSize;
+    public Walrus(int w, double ts){
+        weight = w;
+        tuskSize = ts;
+    }
+}
+public static void main(String[] args){
+    Walrus walrus = new Walrus(3500,10.5);
+    int x = 9;
+    doStuff(Walrus,x);
+    System.out.println(walrus);
+    System.out.println(x);
+}
+public static void doStuff(Walrus W,int x){
+    W.weight = W.weight - 100;
+    x = x - 5;
+}
