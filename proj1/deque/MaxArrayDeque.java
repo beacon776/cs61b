5e719ad29f552028cfe89742779c3582d9d9d3ef
beacon776 <3214984261@qq.com>
2024-11-17 20:49:56 +0800

    Update MaxArrayDeque.java

--- proj1/deque/MaxArrayDeque.java
+++ proj1/deque/MaxArrayDeque.java
@@ -2,22 +2,22 @@ package deque;
 
 import java.util.Comparator;
 
-public class MaxArrayDeque<T> extends ArrayDeque<T>  {
+public class MaxArrayDeque<T> extends ArrayDeque<T> {
 
     private Comparator<T> c;
-    public MaxArrayDeque(Comparator<T> c){
+    public MaxArrayDeque(Comparator<T> c) {
         this.c = c;
     }
 
-    public T max(){
+    public T max() {
         if(isEmpty()) return null;
         return max(c);
     }
 
-    public T max(Comparator<T> c){
+    public T max(Comparator<T> c) {
         if(isEmpty()) return null;
         T max = get(0);
-        for(int i = 1; i < size() ; i++){
+        for(int i = 1; i < size() ; i++) {
             T current = get(i);
             if(c.compare(current,max) > 0) max = current;
         }
